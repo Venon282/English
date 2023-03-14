8e1a1ec168c513909e57d31346ce0ad619eb601c
Venon282 <esto500@hotmail.com>
2023-03-14 22:07:10 +0100

    improvment

--- english.py
+++ english.py
@@ -2,7 +2,7 @@
 """
 Created on Mon Mar 13 20:34:26 2023
 
-@author: esto5
+@author: Vénon
 """
 
 import requests
@@ -13,9 +13,16 @@ import deepl
 import win32com.client as win32
 import ast
 import time
+import random
 
-""""""
+"""
+Todo :
+    -Chose the translation language + the languages of the words to translate
+    -Read me
+"""
     
+#API WORDS
+
 def RDARandoms(n):
     array = []
     for i in range(n):
@@ -40,25 +47,17 @@ def NinjaRandom(api_key):
     if response.status_code == requests.codes.ok:
         return json.loads(response.text)
     return -1
-    
-def loadData(path,operator):
-    dico={}
-    with open(path, 'r') as file:
-        for line in file.readlines():
-            line=line.strip()
-            if line!='':
-                line=line.split(operator)
-                dico[line[0].lower()]=line[1]
-    return dico
 
-def choiceAPI(choice,api_keys,n):
+def choiceAPI(choice,dico,n):
     if choice == 0:
-        return NinjaRandoms(api_keys["ninjas"],n)
+        return NinjaRandoms(dico["ninjas"],n)
     elif choice == 1:
         return RDARandoms(n)
     else:
-        raise ValueError("Choice not good")
+        raise ValueError("Choice API words not good")
         return -1
+    
+#API TRANSLATION
     
 def WordReferences(words):
     wr = WordReference('en', 'fr')
@@ -71,35 +70,34 @@ def WordReferences(words):
             dico[word]=-1
     return dico
 
-def WordReferencesTxt(result):
+def WordReferencesTxt(result,dico):
     content=""
     for word in result:
         if result[word]!=-1:
-            content+=word+" : \n\n"
+            content+="<b>"+word+" : </b><br><br>"
             count = 0
             for item in result[word]["translations"][0]["entries"]:
                 #for item in items["entries"]:
                 for to_word in item["to_word"]:
-                    content+="\t"+to_word["meaning"]+"\n"
+                    content+="&emsp;"+to_word["meaning"]+"<br>"
                 if isinstance(item["from_example"], list):
                     for from_example in item["from_example"]:
-                        content+="\t\t"+from_example+"\n"
+                        content+="&emsp;&emsp;<i>"+from_example+"</i><br>"
                 elif  isinstance(item["from_example"], str):
-                    content+="\t\t"+item["from_example"]+"\n"
+                    content+="&emsp;&emsp;<i>"+item["from_example"]+"</i><br>"
                     
                 if isinstance(item["to_example"], list):
                     for to_example in item["to_example"]:
-                        content+="\t\t"+to_example+"\n\n"
+                        content+="&emsp;&emsp;<i>"+to_example+"</i><br><br>"
                 elif isinstance(item["to_example"], str):
-                    content+="\t\t"+item["to_example"]+"\n\n"
+                    content+="&emsp;&emsp;<i>"+item["to_example"]+"</i><br><br>"
                     
                 count += 1
-                if count == 5:
+                if count == dico["wordreferencemaxexemples"]:
                     break
     content+="\n\n"
     return content
                         
-
 def DeepLs(words,api_key):
     translator = deepl.Translator(api_key) 
     dico={}
@@ -112,33 +110,32 @@ def DeepLs(words,api_key):
 def DeepLsTxt(results):
     content=""
     for result in results:
-        content+=result + " = " + results[result] + "\n"
+        content+=result + " = " + results[result] + "<br><br>"
     return content
 
-def choiceTranslator(choice,api_keys,words):
+def choiceTranslator(choice,dico,words):
     if choice == 0:
-        return WordReferencesTxt(WordReferences(words))
+        return WordReferencesTxt(WordReferences(words),dico)
     elif choice == 1:
-        return DeepLsTxt(DeepLs(words,api_keys["deepl"]))
+        return DeepLsTxt(DeepLs(words,dico["deepl"]))
+    else:
+        raise ValueError("Choice Translator not good")
+        return -1
 
-def apiKeys(dico):
-    api_keys={}
-    ok=False
-    for part in dico:
-        if part=="api key":
-            ok=True
-        if ok == True:
-            api_keys[part]=dico[part]
-    return api_keys
+#EMAIL
 
 def sendOutlookEmail(to, subject, body):
-    #killOutlook()
     outlook = win32.Dispatch('outlook.application')
+    
+        
     mail = outlook.CreateItem(0)
     mail.To = to
     mail.Subject = subject
-    mail.Body = body
+    mail.HTMLBody = body
+    
     mail.Send()
+    
+    del mail  
     time.sleep(10)
     
     killOutlook()
@@ -153,22 +150,112 @@ def killOutlook():
     os.system('taskkill /im outlook.exe /f')
     os.system('reg add HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Office\\16.0\\Outlook\\Security\\ /v ObjectModelGuard /t REG_DWORD /d 0 /f')
 
-def main():
-    dico = loadData( os.path.dirname(os.path.abspath(__file__))+"\data.txt",",")
-    api_keys = apiKeys(dico)
-    words = choiceAPI(int(dico["choiceapi"]),api_keys,int(dico["wordnumber"]))
-    content = choiceTranslator(int(dico["choicetranslator"]),api_keys,words)
+#DATA FILES
+def wordApiTxt(dico):
+    words = choiceAPI(int(dico["choiceapi"]),dico,int(dico["wordnumber"]))
+    content = choiceTranslator(int(dico["choicetranslator"]),dico,words)
+    content = deeplCompletation(dico,content,words)
+    
+    sendEmails(ast.literal_eval(dico["emails"]),content)
+    
+    return True
 
-    if api_keys["deepl"]!="":
+def wordListTxt(dico):
+    with open(dico["wordlist.txt=1"], 'r') as file:
+        words_array = ast.literal_eval(file.read())
+        words=[]
+        size = len(words_array)
+        
+        i=0
+        while i< int(dico["wordnumber"]):
+            n=random.randint(0,size-1)
+            if words_array[n] not in words:
+                words.append(words_array[n])
+                i+=1
+                
+        content = choiceTranslator(int(dico["choicetranslator"]),dico,words)
+        content = deeplCompletation(dico,content,words)
+        
+        sendEmails(ast.literal_eval(dico["emails"]),content)
+        
+        return True
+            
+def wordListTranslateTxt(dico):
+    with open(dico["wordlisttranslate.txt=2"], 'r') as file:
+        words_dico =json.loads(file.read())
+        words=[]
+        
+        i=0
+        while i< int(dico["wordnumber"]):
+            word = random.choice(list(words_dico.keys()))
+            if word not in words:
+                words.append(word)
+                i+=1
+        
+        content=""
+        for word in words:
+            content+=word + " = " + words_dico[word] + "<br><br>"
+        
+        sendEmails(ast.literal_eval(dico["emails"]),content)
+        
+        return True
+
+def choiceData(choice,dico):
+    if choice == 0:
+        return  wordApiTxt(dico)
+    elif choice == 1:
+        return wordListTxt(dico)
+    elif choice == 2:
+        return wordListTranslateTxt(dico)
+    else:
+        raise ValueError("Choice Translator not good")
+        return -1
+
+#GENERAL
+
+def deeplCompletation(dico,content,words):
+    if dico["deepl"]!="":
         retry=[]
         for word in words:
             if isinstance(word, int):
-               retry.append(word)
-          
+                retry.append(word)
+              
         if len(retry)>0:
-            content+=DeepLs(retry,api_keys["deepl"])
+            content+=DeepLs(retry,dico["deepl"])
+    return content
 
-    sendEmails(ast.literal_eval(dico["emails"]),content)
+def loadData(path):
+    dico={}
+    first=True
+    separator=None
+    with open(path, 'r') as file:
+        for line in file.readlines():
+            line=line.strip()
+            if first:
+                if line=='':
+                    raise AttributeError("No separator at the start of the file.")
+                separator=line
+                first=False
+            if line!='':
+                line=line.split(separator)
+                dico[line[0].lower()]=line[1]
+    return dico, separator
+    api_keys={}
+    ok=False
+    for part in dico:
+        if part=="api key":
+            ok=True
+        if ok == True:
+            api_keys[part]=dico[part]
+    return api_keys
+    
+def main():
+    dico, separator = loadData( os.path.dirname(os.path.abspath(__file__))+"\data.txt")
+    data = choiceData(int(dico["choicedata"]),dico)
+
+    
+
+    
 
 if __name__ == "__main__":
     main()
